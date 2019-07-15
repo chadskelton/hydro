3865b22ea329233fd9ac1002872a0131728f70ed
Chad Skelton <cskeltonnews@gmail.com>
2019-07-14 22:47:25 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -112,7 +112,7 @@ def scrape_hydro(url): # in case page changes
 '''
 
 # for x in range (0, 1): # trying 15 instead of 22
-print "Cycle:" + str(x)
+# print "Cycle:" + str(x)
  
 scrape_hydro("https://www.bchydro.com/power-outages/app/outage-list.html#current-521980323")
 # time.sleep(3600) # wait one hour, change this to 3600 seconds
